d6c567d68324f7cd40ef10bf751e2ea9817ee31c
David <felipe0160@gmail.com>
2020-08-24 18:52:08 -0500

    Http requests done

---
+++ src/app/services/spotify.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SpotifyService {
+
+  constructor(private http:HttpClient) { 
+    console.log('hola');
+    
+   }
+
+  getQuery(query:string){
+    const url =`https://api.spotify.com/v1/${query}`;
+    const headers=new HttpHeaders({
+      'Authorization':'Bearer BQDG6PDGoAdijitvH1npbRD51lKORfcWuq_2Guhy0yN-L2LZILWvFVEmszM9DEo7KZde5wDMqiA4ksSXB7'
+    });
+
+    return this.http.get(url,{headers});
+  }
+   
+  getNewReleases(){
+    return this.getQuery('browse/new-releases')
+            .pipe(map(data=> data['albums'].items));
+  }
+
+  getArtistas(termino:string){
+    return this.getQuery(`search?q=${termino}&type=artist&limit=15`)
+              .pipe(map(data=>data['artists'].items));
+  }
+
+  getArtista(id:string){
+    return this.getQuery(`artists/${id}`);
+  }
+
+  getTopTracks(id:string){
+    return this.getQuery(`artists/${id}/top-tracks?country=us`)
+              .pipe(map(data=>data['tracks']));
+  }
+}
